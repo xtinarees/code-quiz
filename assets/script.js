6208ba056a15180d9864757965619a062fd0a4fc
Alex Macon <alexdmacon@gmail.com>
2021-08-24 22:25:42 -0500

    Started messing around with html and css. Compiling references and links in .js file that should prove helpful later.

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+//https://codingnepal.medium.com/create-a-quiz-app-with-timer-using-html-css-javascript-f460087a88a1
+//https://stackoverflow.com/questions/45449106/how-to-make-a-ul-list-of-buttons-accessible/45491521
+// https://www.codingnepalweb.com/quiz-app-with-timer-javascript/
+// https://codepen.io/boopalan002/pen/yKZVGa
