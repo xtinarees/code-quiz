dfac209d4bacdd1bd6ec31c3efeaa5833b71f22a
Alex Macon <alexdmacon@gmail.com>
2021-08-27 06:56:01 -0500

    Creating more variables in js. Messing around with function that will generate quiz questions and answers.

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@
 // https://www.codingnepalweb.com/quiz-app-with-timer-javascript/
 // https://codepen.io/boopalan002/pen/yKZVGa
 // https://www.w3schools.com/js/js_quiz.asp
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+
+
 
 var questions = [
     {
@@ -28,33 +31,71 @@ var questions = [
     }
 ];
 
+console.log(questions[0].question);
+
+
 var welcome = document.querySelector(".welcome");
 var startButton = document.querySelector(".start-button");
 var quitButton = document.querySelector(".quit-button");
 var quiz = document.querySelector(".quiz");
-var timerEl = document.querySelector(".timer-text")
+var timerEl = document.querySelector(".timer-text");
+var answers = document.querySelector(".answers");
+var questionEl = document.querySelector(".questions");
+var buttonA = document.querySelector(".buttonA");
+var buttonB = document.querySelector(".buttonB");
+var buttonC = document.querySelector(".buttonC");
+var buttonD = document.querySelector(".buttonD");
+var questionNumber = 0;
+var answers = document.querySelector(".answers")
 
 
 
-
-
+// Starts the game when user clicks start button
 startButton.addEventListener("click", startGame);
 
+// 
+quitButton.addEventListener("click", quitGame);
 
+// Sets everything in motion and hides welcome text
 function startGame(){
     startTimer();
-    document.getElementsByClassName("welcome")[0].style.visibility = "hidden";
+    document.getElementsByClassName("welcome")[0].style.visibility="hidden";
+    questionNumber = 0;
+    createQuestions(questionNumber);
+}
+
+
+// Creates questions and answers by pulling from arrays
+function createQuestions(questionNumber) {
+    if (questionNumber < questions.length) {
+        questionEl.textContent = questions[questionNumber].question;
+        buttonA.textContent = questions[questionNumber].choices[0];
+        buttonB.textContent = questions[questionNumber].choices[1];
+        buttonC.textContent = questions[questionNumber].choices[2];
+        buttonD.textContent = questions[questionNumber].choices[3];
+    }
+
 
 
 }
 
+
+
+// Text appears when user loses game by running out of time
 function loseGame() {
+    quiz.textContent = "You ran out of time! You really don't have what it takes to be The Lord of the Code. Refresh the page and try again, but be quick this time."
+}
 
+// Text appears when user clicks quit button
+function quitGame() {
+    welcome.textContent = "What? You're not even gonna try? Come on. Refresh the page and try."
 }
 
 
+
+// Controls the timer
 function startTimer() {
-    var timeLeft = 40;
+    var timeLeft = 10;
     var timeInterval = setInterval(function(){
         if (timeLeft > 1) {
             timerEl.textContent = timeLeft + " seconds remaining";
